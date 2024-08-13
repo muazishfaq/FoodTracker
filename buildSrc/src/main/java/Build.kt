604428d3ef3fa5f439d3b4a30374798fdfb06d78
Muaz Ishfaq <muaz.ishfaq@gmail.com>
2024-08-13 20:43:59 +0200

    gradle setup

---
+++ buildSrc/src/main/java/Build.kt
@@ -0,0 +1,12 @@
+object Build {
+    private const val androidBuildToolsVersion = "8.1.1"
+    const val androidBuildTools = "com.android.tools.build:gradle:$androidBuildToolsVersion"
+
+    const val kotlinGradlePlugin = "org.jetbrains.kotlin:kotlin-gradle-plugin:${Kotlin.version}"
+
+    private const val hiltAndroidGradlePluginVersion = "2.44"
+    const val hiltAndroidGradlePlugin = "com.google.dagger:hilt-android-gradle-plugin:$hiltAndroidGradlePluginVersion"
+
+    private const val jdkLibVersion = "1.1.5"
+    const val jdkLibPlugin = "com.android.tools:desugar_jdk_libs:$jdkLibVersion"
+}
---
+++ buildSrc/src/main/java/Kotlin.kt
@@ -0,0 +1,4 @@
+object Kotlin {
+    const val version = "1.8.22"
+    const val kotlinStdVersion = "org.jetbrains.kotlin:kotlin-stdlib:$version"
+}
